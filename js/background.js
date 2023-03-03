adc95f49d94fe6df070c0aef89df5b95c4a0ee84
lifedesigner88 <lifedesigner88@gmail.com>
2023-03-03 20:55:16 +0900

    complited by img

--- js/background.js
+++ js/background.js
@@ -1,10 +1,6 @@
 const images = [
-  "http://www.desktopwallpaperhd.net/wallpapers/3/d/nature-ivanandreevich-bellevue-room-wallpaper-38877.jpg",
-  "http://www.desktopwallpaperhd.net/wallpapers/4/9/wallpapers-mountains-filter-random-nature-nuahs-glacial-high-resolutions-room-40425.jpg",
-  "http://www.desktopwallpaperhd.net/wallpapers/1/a/mountains-green-wallpapers-walls-resolutions-19514.jpg",
-  "http://www.desktopwallpaperhd.net/wallpapers/0/8/autumn-wallpaper-beauty-landscape-plant-source-7064.jpg",
-  "http://www.desktopwallpaperhd.net/wallpapers/1/e/windows-theme-official-autumn-canada-wallpaper-themes-maple-leaves-gallery-rocks-15470.jpg",
-  "http://www.desktopwallpaperhd.net/wallpapers/1/6/water-board-autumn-background-summer-nature-wallpaper-16550.jpg",
+  "https://picsum.photos/1200/800/?random",
+  "https://picsum.photos/1200/900/?random",
 ];
 
 function changeAll() {
